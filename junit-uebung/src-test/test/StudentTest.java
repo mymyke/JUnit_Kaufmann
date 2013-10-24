bd5744d7b5f8ccfeb9f9877d1d094848e91aba88
mymyke <michi.kaufmann1012@gmail.com>
2013-10-24 19:02:28 +0200

    Fertig

--- junit-uebung/src-test/test/StudentTest.java
+++ junit-uebung/src-test/test/StudentTest.java
@@ -18,13 +18,30 @@ public class StudentTest {
 		assertSame("H", s.getFirstName());
 		assertSame("B", s.getLastName());
 
-		s = new Student("Klaus   ", "Unger   ");
+		s = new Student("Klaus", "Unger");
 		assertEquals("Klaus", s.getFirstName());
 		assertEquals("Unger", s.getLastName());
 	}
+	@Test
+	public void constructorWithValidArgumentsFalse() {
+		Student s = new Student("F", "D");
+		assertSame("H", s.getFirstName());
+		assertSame("B", s.getLastName());
+
+		s = new Student("S  ", "D   ");
+		assertEquals("Klaus", s.getFirstName());
+		assertEquals("Unger", s.getLastName());
+	}
+	@Test
 	public void TestCompareTo(){
 		Student o=new Student("hans","Berg",true);
 		Student s= new Student("hans","Berger",true);
+		assertEquals(s.compareTo(o), 0);
+	}
+	@Test
+	public void TestCompareToFalse(){
+		Student o=new Student("hans","Berg",true);
+		Student s= new Student("Karl","Peter",true);
 		assertEquals(s.compareTo(o), 0);
 	}
 	
@@ -35,5 +52,13 @@ public class StudentTest {
 		assertEquals("Student {firstName=Klaus, lastName=Unger, selfEntitled=true}",
 				s.toString());
 	}
+	@Test
+	public void testToStringFalse() {
+
+		Student s = new Student("Klaus", "Unger",true);
+		assertEquals("Student {firstName=Peter, lastName=Unger, selfEntitled=true}",
+				s.toString());
+	}
+
 
 }
