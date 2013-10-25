016527f794f70d3f5ab96131f1469a99efd474c5
mymyke <michi.kaufmann1012@gmail.com>
2013-10-25 12:16:31 +0200

    Done

--- junit-uebung/src/daten/Course.java
+++ junit-uebung/src/daten/Course.java
@@ -1,12 +1,45 @@
 package daten;
+import java.util.Comparator;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class Course {
 
-	private String name;
 
-	private final Set<Student> students = new TreeSet<Student>();
+	public Course(String name) {
+        setName(name);
+	}
+	
+	public String getName() {
+	        return name;
+	}
+	
+	public void setName(String name) {
+	        if (name == null) {
+	                throw new IllegalArgumentException("name must not be null!");
+	        }
+	        if ("".equals(name.trim())) {
+	                throw new IllegalArgumentException(
+	                                "name must not empty or whitespaces only!");
+	        }
+	        this.name = name;
+	}
+		
+		private String name;
+		
+		private final Set<Student> students = new TreeSet<Student>(new Comparator<Student>() {
+				@Override
+		        public int compare(Student o1, Student o2) {
+		                int comp = 0;
+		                if((comp = o1.getLastName().compareTo(o2.getLastName()))==0) {
+		                        if((comp = o1.getFirstName().compareTo(o2.getFirstName()))==0) {
+		                                return o1.getBirthDate().compareTo(o2.getBirthDate());
+		                        }
+		                }
+		                return comp;
+		        }
+		});
+
 
 	public void addStudent(Student student) {
 		if (student == null) {
